86c115e6725d94cd7b0036fa89bdafa3381e77e0
AlemolAMG <alemolamg@outlook.com>
2022-05-25 21:44:34 +0200

    AÑADIDO NAVEGACIÓN

--- levelWare/resources/views/navigation-menu.blade.php
+++ levelWare/resources/views/navigation-menu.blade.php
@@ -21,6 +21,12 @@
                         {{ __('Productos') }}
                     </x-jet-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('consola.index') }}" :active="request()->routeIs('consola')">
+                        {{ __('Consolas') }}
+                    </x-jet-nav-link>
+                </div>
+
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
