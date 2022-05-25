9d15ce0dab63ffc63bd1a84c34718b99b6a5a051
AlemolAMG <alemolamg@outlook.com>
2022-05-25 21:44:18 +0200

    UP productos

--- levelWare/resources/views/components/product-list-component.blade.php
+++ levelWare/resources/views/components/product-list-component.blade.php
@@ -1,16 +1,17 @@
 <?php
-$productos = \App\Models\Product::all();
+$productos = \App\Models\Product::all()->take(5);
 ?>
 
-<div class="rounded-lg overflow-hidden md:mx-2 my-2 bg-red-300 shadow-md">  <!-- bg-{$color}-200"> -->
+<div class="rounded-lg overflow-hidden md:mx-2 my-2 bg-red-300 shadow-md">
+    <!-- bg-{$color}-200"> -->
     <h2 class="mx-2 mt-1 text-2xl px-2">Lista productos </h2>
     <div id="boxCartas" class="grid grid-cols-2 md:grid-cols-5 justify-center pt-1">
 
-        @foreach($productos as $pro)
-            <x-productCard id="{{$pro->id}}">
-                <x-slot name="imagen">{{$pro->imagen}}</x-slot>
-                <x-slot name="titulo">{{$pro->nombre}}</x-slot>
-                {{$pro->descripcion}}
+        @foreach ($productos as $pro)
+            <x-productCard id="{{ $pro->id }}">
+                <x-slot name="imagen">{{ $pro->imagen }}</x-slot>
+                <x-slot name="titulo">{{ $pro->nombre }}</x-slot>
+                {{ $pro->descripcion }}
             </x-productCard>
         @endforeach
 
