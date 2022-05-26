df8f50d1ab23873927e58c8e6f34e432f0a502c5
AlemolAMG <alemolamg@outlook.com>
2022-05-26 21:49:18 +0200

    Pequeño cambio de color para las tarjetas de productos.

--- levelWare/resources/views/components/product-card.blade.php
+++ levelWare/resources/views/components/product-card.blade.php
@@ -1,4 +1,4 @@
-<a class="transition hover:-translate-y-1 hover:scale-105 hover:bg-emerald-300 duration-300  max-w-sm mx-2 my-2 rounded-lg overflow-hidden md:mx-2 my-2 bg-indigo-300"
+<a class="transition hover:-translate-y-1 hover:scale-105 hover:bg-emerald-300 duration-300  max-w-sm mx-2 my-2 rounded-lg overflow-hidden md:mx-2 my-2 bg-indigo-200"
     href="{{ route('producto.show', $id) }}">
     <!-- bg-{$color}-200"> -->
     <img class="w-full" src="{{ $imagen }}" alt="Imagen del producto">
