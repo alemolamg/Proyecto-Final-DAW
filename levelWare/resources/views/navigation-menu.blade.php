c4276e9eeae51ff18d11091e8c7b3006ca484d20
AlemolAMG <alemolamg@outlook.com>
2022-05-26 21:56:12 +0200

    ADD cambios MENORES

--- levelWare/resources/views/navigation-menu.blade.php
+++ levelWare/resources/views/navigation-menu.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('PEPE') }}
+                        {{ __('Menú principal') }}
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
@@ -165,6 +165,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-jet-responsive-nav-link href="{{ route('consola.index') }}" :active="request()->routeIs('consola')">
+                {{ __('Consolas') }}
             </x-jet-responsive-nav-link>
         </div>
 
