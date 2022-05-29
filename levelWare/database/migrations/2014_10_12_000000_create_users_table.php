be125e34539b739dc5968163054102e0fada6840
AlemolAMG <alemolamg@outlook.com>
2022-05-29 23:36:47 +0200

    up MIGRATION

--- levelWare/database/migrations/2014_10_12_000000_create_users_table.php
+++ levelWare/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -16,9 +17,10 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('apellidos');
-            $table->string('email')->unique();
+            $table->string('email')->º();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('rol');
             $table->string('direccion')->nullable();
             $table->string('creditCard')->nullable();
             $table->rememberToken();
