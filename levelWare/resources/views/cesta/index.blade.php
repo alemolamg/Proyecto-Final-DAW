9dd9c0b021fc2938ab4b77cde23b84b649f30056
AlemolAMG <alemolamg@outlook.com>
2022-06-07 23:40:13 +0200

    ADD pequeños cambios

--- levelWare/resources/views/cesta/index.blade.php
+++ levelWare/resources/views/cesta/index.blade.php
@@ -4,22 +4,16 @@
 
 <x-app-layout>
     <div class="py-0">
-        <div class="max-w mx-auto sm:px-0 lg:px-0">
-            <div class="bg-white overflow-hidden ">
-                <div class="p-3 bg-green-700">
+        <div class="max-w mx-auto sm:px-0 lg:px-0 overflow-hidden levelware">
+            <div class="p-3">
 
-                    @if ((session()->get('CESTA')) == NULL)
-                        {{'La cesta está vacía'}}
-                    @else
-                        <!-- Muestra los elementos de la cesta ordenados por lista -->
-                        <x-CartList></x-CartList>
-                    @endif
+                @if (session()->get('CESTA') == null)
+                    <div class="text-center font-bold text-2xl py-2">La cesta está vacía </div>
+                @else
+                    <!-- Muestra los elementos de la cesta ordenados por lista -->
+                    <x-CartList></x-CartList>
+                @endif
 
-                </div>
-                <a href="#">
-                    <input type="submit" value="Comprar todo"
-                           class="bg-indigo-600 hover:bg-green-300 hover:text-black text-white font-bold py-2 px-4 rounded">
-                </a>
             </div>
         </div>
 </x-app-layout>
