3068fa3380fba8c5493687f11fb6b963aebfbd46
AlemolAMG <alemolamg@outlook.com>
2022-06-07 22:24:32 +0200

    Mejora rendimiento + nueva interfaz en negro

--- levelWare/resources/views/components/cart-list.blade.php
+++ levelWare/resources/views/components/cart-list.blade.php
@@ -2,7 +2,7 @@
 $arrayCesta = session()->get('CESTA');
 ?>
 
-<div class="grid grid-flow-col text-center align-content-center">
+<div class="grid grid-flow-col text-center align-content-center levelware">
     <table class="table-fixed">
         <thead>
             <tr>
@@ -16,19 +16,20 @@ $arrayCesta = session()->get('CESTA');
         <tbody>
 
             @foreach ($arrayCesta as $pc)
+                <!-- Buscamos el producto para sacar los datos -->
+                <?php $proc = App\Models\Product::find($pc['idPro']); ?>
                 <tr class="border-solid border-2 border-white">
                     <!-- <td>{var_dump($pc) }</td> -->
                     <td>
-                        <a href="{{ route('producto.show', App\Models\Product::find($pc['idPro'])['id']) }}">
-                            <img class="w-20 items-center" src="{{ App\Models\Product::find($pc['idPro'])['imagen'] }}"
-                                alt="{{ App\Models\Product::find($pc['idPro'])['imagen'] }}">
+                        <a href="{{ route('producto.show', $proc->id) }}">
+                            <img class="w-20 items-center" src="{{ $proc->imagen }}" alt="{{ $proc->imagen }}">
                         </a>
                     </td>
                     <td>
-                        <a href="{{ route('producto.show', App\Models\Product::find($pc['idPro'])['id']) }}">
-                            {{ App\Models\Product::find($pc['idPro'])['nombre'] }} </a>
+                        <a href="{{ route('producto.show', $proc->id) }}">
+                            {{ $proc->nombre }} </a>
                     </td>
-                    <td>{{ App\Models\Product::find($pc['idPro'])['precio'] }}</td>
+                    <td>{{ $proc->precio }}</td>
                     <td>{{ $pc['cant'] }}</td>
                     <td>
                         <btn>btn 1</btn>
@@ -36,8 +37,13 @@ $arrayCesta = session()->get('CESTA');
                     </td>
                 </tr>
             @endforeach
-
-
         </tbody>
     </table>
+
+</div>
+<div class="p-2">
+    <a href="#">
+        <input type="submit" value="Comprar todo"
+            class="bg-indigo-600 hover:bg-green-300 hover:text-black text-white font-bold py-2 px-4 rounded">
+    </a>
 </div>
