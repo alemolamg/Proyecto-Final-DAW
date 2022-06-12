b9c6aed205d0d7de9cc67acb7a33db0877448a6b
AlemolAMG <alemolamg@outlook.com>
2022-06-12 15:06:29 +0200

    Cambios hasta la comida

--- levelWare/resources/views/components/cart-list.blade.php
+++ levelWare/resources/views/components/cart-list.blade.php
@@ -1,5 +1,6 @@
 <?php
 $arrayCesta = session()->get('CESTA');
+$precioTotal = 0;
 ?>
 
 <div class="grid grid-flow-col text-center align-content-center levelware">
@@ -17,7 +18,10 @@ $arrayCesta = session()->get('CESTA');
 
             @foreach ($arrayCesta as $key => $pc)
                 <!-- Buscamos el producto para sacar los datos -->
-                <?php $proc = App\Models\Product::find($pc['idPro']); ?>
+                <?php
+                $proc = App\Models\Product::find($pc['idPro']);
+                $precioTotal += $proc->precio * $pc['cant'];
+                ?>
                 <tr class="border-solid border-2 border-white">
                     <!-- <td>{var_dump($pc) }</td> -->
                     <td>
@@ -29,16 +33,20 @@ $arrayCesta = session()->get('CESTA');
                         <a href="{{ route('producto.show', $proc->id) }}">
                             {{ $proc->nombre }} </a>
                     </td>
-                    <td>{{ $proc->precio }}</td>
+                    <td>{{ $proc->precio }}€</td>
                     <td>{{ $pc['cant'] }}</td>
+
+                    <!-- BTN para eliminar producto de la cesta -->
                     <td>
                         <form action="#" style="display: block" id="formEliminar">
                             <input type="button" onclick="verCesta()" value="Eliminar"
                                 class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">
                         </form>
+
                         <div id="kk">{{ $key }}</div>
-                        <form action="#" style="display: block" id="formActualizar">
-                            <input type="button" onclick="verCesta()" value="Actualizark"
+
+                        <form action="#" style="display: none" id="formActualizar">
+                            <input type="button" onclick="verCesta()" value="Actualizar"
                                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
                         </form>
 
@@ -50,12 +58,24 @@ $arrayCesta = session()->get('CESTA');
 
 </div>
 @if (!empty($arrayCesta))
-    <div class="p-2">
-        <form method="POST" action="{{ route('pedido.store') }}">
-            @csrf
-            <input type="submit" value="Comprar todo"
-                class="bg-indigo-600 hover:bg-green-300 hover:text-black text-white font-bold py-2 px-4 rounded">
-        </form>
+    <div class="p-2 grid grid-cols-4 align-content-end">
+        <div></div>
+        <div id="divPrecio" class="text-bold text-uppercase">
+            <p class="text-bold text-uppercase">Precio total: {{ $precioTotal }}€ </p>
+        </div>
+        <div>
+            <form action="#" id="vaciarCesta">
+                <input type="submit" value="Vaciar Cesta"
+                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">
+            </form>
+        </div>
+        <div id="divBTN">
+            <form method="POST" action="{{ route('pedido.store') }}">
+                @csrf
+                <input type="submit" value="Comprar todo"
+                    class="bg-indigo-600 hover:bg-green-300 hover:text-black text-white font-bold py-2 px-4 rounded">
+            </form>
+        </div>
     </div>
 @endif
 
