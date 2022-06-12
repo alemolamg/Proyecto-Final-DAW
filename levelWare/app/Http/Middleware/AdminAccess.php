48371df8bac754992c3cb66f3148af54c14a58f1
AlemolAMG <alemolamg@outlook.com>
2022-06-12 21:10:34 +0200

    Middleware de administrador creado

--- levelWare/app/Http/Middleware/AdminAccess.php
+++ levelWare/app/Http/Middleware/AdminAccess.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAccess
 {
@@ -16,6 +18,8 @@ class AdminAccess
      */
     public function handle(Request $request, Closure $next)
     {
+        if (Auth::user()->rol == 0)
+            return redirect('/');
         return $next($request);
     }
 }
