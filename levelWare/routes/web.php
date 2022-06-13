2293bc27a29876689b902645e1216300736faa3b
AlemolAMG <alemolamg@outlook.com>
2022-06-13 18:29:38 +0200

    Arreglada ruta cesta.
    La ruta a la cesta tenía un espacio y no funcionaba

--- levelWare/routes/web.php
+++ levelWare/routes/web.php
@@ -28,7 +28,7 @@ Route::middleware([
 ])->group(function () {
     Route::post('/aniadirCestaSesion', [cestaController::class, 'aniadirCestaSesion'])->name('aniadirCestaSesion');
     Route::put('/eliminarProducto', [cestaController::class, 'eliminarDeCesta'])->name('eliminarProducto');
-    Route::post('/vaciarCesta', [cestaController::class, 'vaciarCesta'])->name('vaciarCesta ');
+    Route::post('/vaciarCesta', [cestaController::class, 'vaciarCesta'])->name('vaciarCesta');
 });
 
 /**
